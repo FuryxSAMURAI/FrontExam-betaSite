b0d7936ff056cd870f0cfae43713da8dc3b6638c
FuryxSAMURAI <111450833+FuryxSAMURAI@users.noreply.github.com>
2023-03-16 14:58:21 +0200

    beta v2.7

--- js/main.js
+++ js/main.js
@@ -99,11 +99,83 @@ card = `{
       "price":"19 190 грн"
     }
   },
+  "btnAdsText":"Показати увесь товар",
   "ads":{
       "img":"assets/images/ads/photo-1441986300917-64674bd600d8.jpg",
       "title":"До <span>-20%</span> якщо зареєструєшся <br> на нашому сайті!",
       "btnCont":"Детальніше"
-  }
+  },
+  "trendsTitle":"Зараз у тренді",
+  "cardTrends":{
+    "1":{
+      "cardImg":"assets/images/trends/cap/15cf95285117fd50034cd322f362.webp",
+      "cardText":"<a href='#'>Кепки</a>"
+    },
+    "2":{
+      "cardImg":"assets/images/trends/tShirt/ae0f8af5d93766560407566613d3.webp",
+      "cardText":"<a href='#'>Футболки</a>"
+    },
+    "3":{
+      "cardImg":"assets/images/trends/shorts/cd413a060d102b003cd752585119.webp",
+      "cardText":"<a href='#'>Шорти</a>"
+    },
+    "4":{
+      "cardImg":"assets/images/trends/shoe/3c3a79027102bf7e10c5cf777bf5.webp",
+      "cardText":"<a href='#'>Обувь</a>"
+    }
+  },
+  "brands":{
+    "1":{
+      "text":"MEXX"
+    },
+    "2":{
+      "text":"UNDER ARMOUR"
+    },
+    "3":{
+      "text":"TOMMY HILFIGER"
+    },
+    "4":{
+      "text":"DIESEL"
+    },
+    "5":{
+      "text":"CALVIN KLEIN"
+    },
+    "6":{
+      "text":"HELEN MARLEN"
+    }
+  },
+  "popularCLotheTitle":"Зараз популярне",
+  "popularCLothe":{
+    "1":{
+      "cardImg":"assets/images/adsClothe/clothe1/9f3db00d358ce9d02b6803a6a1ef.webp",
+      "cardBrand":"under armour",
+      "cardName":"Чоловічі чорні шорти UA Vanish Woven 8in Shorts",
+      "cardPriceWithDiscount":"1 695 грн",
+      "cardPrice":"2 290 грн"
+    },
+    "2":{
+      "cardImg":"assets/images/adsClothe/clothe2/b3bb52b16c8da897080cbe2742c5.jpg",
+      "cardBrand":"Preach",
+      "cardName":"Жовті шорти Essential Sweat Shorts",
+      "cardPriceWithDiscount":"1 695 грн",
+      "cardPrice":"3 390 грн"
+    },
+    "3":{
+      "cardImg":"assets/images/adsClothe/clothe3/d02b7a4b50d6637e34d2ea87ea5d.webp",
+      "cardBrand":"Tommy Jeans",
+      "cardName":"Жіноча бордова футболка TJW RLXD TOMMY SIGNATURE",
+      "cardPriceWithDiscount":"685 грн",
+      "cardPrice":"1 990 грн"
+    },
+    "4":{
+      "cardImg":"assets/images/adsClothe/clothe4/a2287d543137579cdea2daaf6ffb.webp",
+      "cardBrand":"Under Armour",
+      "cardName":"Дитячі чорні шорти UA Prototype 2.0 Wdmk",
+      "cardPriceWithDiscount":"476 грн",
+      "cardPrice":"1 190 грн"
+    }
+  },
+  "btnPopularText":"Показати весь одяг"
 }`
 let jsParsed = JSON.parse(card)
 
@@ -153,15 +225,14 @@ for (let i = 1; i <= 4; i++) {
       cardsInfo.appendChild(cardsName)
       cardsInfo.appendChild(cardsPriceWithDis)
       cardsPriceWithDis.appendChild(cardsPrice)
+      
     }
+
     // ! ADS
     let adsImg = jsParsed.ads.img;
     let adsTitle = jsParsed.ads.title;
     let adsBtn = jsParsed.ads.btnCont;
-    console.log(adsImg);
-    console.log(adsTitle);
-    console.log(adsBtn);
-
+    
     let adsContainer = document.createElement('div')
     adsContainer.classList.add('main__container-ads-container')
     let adsWrapper = document.createElement('div')
@@ -179,8 +250,8 @@ for (let i = 1; i <= 4; i++) {
     adsInfoSubBtn.classList.add('main__container-ads-wrapper-subtitle-btn')
     let adsInfoBtn = document.createElement('button')
     adsInfoBtn.innerHTML = adsBtn;
-
-
+    
+    
     outMainContainer.append(adsContainer)
     adsContainer.appendChild(adsWrapper)
     adsWrapper.appendChild(adsImgWrapper)
@@ -188,3 +259,99 @@ for (let i = 1; i <= 4; i++) {
     adsWrapper.appendChild(adsInfoTitle)
     adsInfoTitle.appendChild(adsInfoSubBtn)
     adsInfoSubBtn.appendChild(adsInfoBtn)
+    
+    let trendsTitle = document.createElement("div");
+    let trends = document.createElement("div");
+    trendsTitle.classList.add('main__container-trends-title')
+    trendsTitle.innerHTML = jsParsed.trendsTitle;
+    outMainContainer.appendChild(trendsTitle)
+    
+    // ! TRENDS 
+    
+    let trendsCont = document.createElement("div");
+    trendsCont.classList.add('main__container-trends-container')
+    let trendsWrapper = document.createElement("div");
+    trendsWrapper.classList.add('main__container-trends-wrapper')
+    outMainContainer.appendChild(trendsCont)
+    trendsCont.appendChild(trendsWrapper)
+    for (let i = 1; i <= 4; i++) {
+      let trendsCards = document.createElement("div");
+      trendsCards.classList.add('main__container-trends-container-cards')
+      let trendsCard = document.createElement("div");
+      trendsCard.classList.add('main__container-trends-container-card')
+      let trendsCardImg = document.createElement("div");
+      trendsCardImg.classList.add('main__container-trends-container-cards-img')
+      let trendsImg = document.createElement("img");
+      trendsImg.src = jsParsed.cardTrends[i].cardImg;
+      let trendsCardTitle = document.createElement("div");
+      trendsCardTitle.classList.add('main__container-trends-container-cards-title')
+      trendsCardTitle.innerHTML = jsParsed.cardTrends[i].cardText
+      
+      trendsWrapper.append(trendsCards)
+      trendsCards.appendChild(trendsCard)
+      trendsCard.appendChild(trendsCardImg)
+      trendsCardImg.appendChild(trendsImg)
+      trendsCard.appendChild(trendsCardTitle)
+    }
+    
+    // ! BRANDS
+    // let brandsCont = document.createElement('div')
+    // brandsCont.classList.add('main__container-slider-brands-container')
+    // let brandsWrapper = document.createElement('div')
+    // brandsWrapper.classList.add('main__container-slider-brands-wrapper')
+
+    // for (let i = 1; i <= 6; i++) {
+    //   let brandsText = document.createElement('div');
+    //   brandsText.classList.add('main__container-slider-brands-text')
+    //   brandsText.innerHTML = jsParsed.brands[i].text;
+    //   brandsWrapper.appendChild(brandsText);
+      
+    // }
+    
+    // outMainContainer.appendChild(brandsCont);
+    // brandsCont.appendChild(brandsWrapper);
+    
+    // ! POPULAR CLOTHES
+    let popularClotheTitle = jsParsed.popularCLotheTitle;
+    let popularCont = document.createElement('div');
+    popularCont.classList.add('main__container-ads-container-clothe')
+    let popularWrapper = document.createElement('div');
+    popularWrapper.classList.add('main__container-ads-wrapper-clothe')
+    
+    outMainContainer.appendChild(popularCont);
+    popularCont.appendChild(popularWrapper);
+    popularCont.appendChild(popularWrapper);
+    for (let i = 1; i <=4; i++) {
+      let popularCards = document.createElement('div');
+      popularCards.classList.add('main__container-ads-wrapper-clothe-card')
+      let popularCardImgCont = document.createElement('div');
+      popularCardImgCont.classList.add('main__container-ads-wrapper-clothe-img')
+      let popularCardImg = document.createElement('img');
+      popularCardImg.src = jsParsed.popularCLothe[i].cardImg
+      let popularCardBrand = document.createElement('div');
+      popularCardBrand.innerHTML = jsParsed.popularCLothe[i].cardBrand;
+      popularCardBrand.classList.add('main__container-ads-wrapper-clothe-brand-title')
+      let popularCardName = document.createElement('div');
+      popularCardName.innerHTML = jsParsed.popularCLothe[i].cardName;
+      popularCardName.classList.add('main__container-ads-wrapper-clothe-title')
+      let popularCardPriceWithDiscount = document.createElement('div');
+      popularCardPriceWithDiscount.innerHTML = jsParsed.popularCLothe[i].cardPriceWithDiscount;
+      popularCardPriceWithDiscount.classList.add('main__container-ads-wrapper-clothe-price')
+      let popularCardPrice = document.createElement('span');
+      popularCardPrice.innerHTML = jsParsed.popularCLothe[i].cardPrice;
+      
+      popularWrapper.appendChild(popularCards)
+      popularCards.appendChild(popularCardImgCont)
+      popularCardImgCont.appendChild(popularCardImg)
+      popularCards.appendChild(popularCardBrand)
+      popularCards.appendChild(popularCardName)
+      popularCards.appendChild(popularCardPriceWithDiscount)
+      popularCardPriceWithDiscount.appendChild(popularCardPrice)
+    }
+    let btnShowMorePopularClothe = document.createElement('div');
+    btnShowMorePopularClothe.classList.add('main__container-ads-wrapper-clothe-button')
+    let btnShowPopular = document.createElement('button')
+    btnShowPopular.innerHTML = jsParsed.btnPopularText;
+
+    outMainContainer.appendChild(btnShowMorePopularClothe)
+    btnShowMorePopularClothe.appendChild(btnShowPopular)
